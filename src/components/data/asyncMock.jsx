b00f64f5b0da488b6a9f62416caa0094715aa4ea
dayanaher <165041586+dayanaher@users.noreply.github.com>
2024-11-05 11:01:38 -0300

    Update asyncMock.jsx

--- src/components/data/asyncMock.jsx
+++ src/components/data/asyncMock.jsx
@@ -1,35 +1,43 @@
+import {renderToString} from "react-dom/server.js";
+
 export const products = [
     {
         id: 1,
-        name: 'stone cup',
+        name: 'Manzana',
         price: 299,
         despcription: 'Lorem ipsum dolor sit amet',
         stock: 5,
-        img: 'https://demos.webicode.com/html/eco-shop-html/html/images/product-2-1.jpg'
+        img: 'https://demos.webicode.com/html/eco-shop-html/html/images/product-2-1.jpg',
+        category: 'Frutas',
+
     },
     {
         id: 2,
-        name: 'gray bag',
+        name: 'Zanahoria',
         price: 300,
         despcription: 'Lorem ipsum dolor sit amet',
         stock: 5,
-        img: 'https://demos.webicode.com/html/eco-shop-html/html/images/product-2-2.jpg'
+        img: 'https://demos.webicode.com/html/eco-shop-html/html/images/product-2-2.jpg',
+        category: 'Verduras',
+
     },
     {
         id: 3,
-        name: 'chiar',
+        name: 'Aceite de oliva',
         price: 310,
         despcription: 'Lorem ipsum dolor sit amet',
         stock: 5,
-        img: 'https://demos.webicode.com/html/eco-shop-html/html/images/product-2-3.jpg'
+        img: 'https://demos.webicode.com/html/eco-shop-html/html/images/product-2-3.jpg',
+        category: 'Aceites',
     },
     {
         id: 4,
-        name: 'STool',
+        name: 'Porotos',
         price: 310,
         despcription: 'Lorem ipsum dolor sit amet',
         stock: 5,
-        img: 'https://demos.webicode.com/html/eco-shop-html/html/images/product-2-4.jpg'
+        img: 'https://demos.webicode.com/html/eco-shop-html/html/images/product-2-4.jpg',
+        category: 'Legumbres',
     },
 ]
 
@@ -40,3 +48,21 @@ export const getProducts = () =>{
         }, 2000);
     })
 }
+
+export const getProductById = (id) => {
+    return new Promise((res) => {
+        const productoFiltrado = products.find((product) => product.id === parseInt(id));
+        setTimeout(() => {
+            res(productoFiltrado);
+        }, 2000);
+    });
+};
+
+export const getProductByCategory = (category) => {
+    return new Promise((res) => {
+        const productosFiltrados = products.filter((product) => product.category === category);
+        setTimeout(() => {
+            res(productosFiltrados);
+        }, 2000);
+    });
+};
